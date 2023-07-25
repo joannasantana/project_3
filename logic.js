952b1b06564e7b73056a2b3a8791055c9e82fc88
aarschne <118419071+aarschne@users.noreply.github.com>
2023-07-25 09:42:06 +0800

    Got a working list of top 10

--- logic.js
+++ logic.js
@@ -1,6 +1,27 @@
 d3.json("Output/cases.json").then(function(data) {
-   let cases = data
+   var cases = data
+   var casesList2021 = []
     console.log(data)
+    for(var i in cases){
+        var key = i;
+        var caseAmount = cases[i]["Years"]["2021"]
+        let currentDict = {
+            "country" : key,
+            "numCases" : caseAmount
+        }
+        casesList2021.push(currentDict)
+    }
+    casesList2021.sort(function(a,b){
+        return b.numCases - a.numCases;
+    })
+    console
+    let list = document.getElementById("top10list");
+    for(let i = 0; i < 10; i++){
+        let li = document.createElement("li");
+        li.appendChild(document.createTextNode(casesList2021[i]["country"]));
+        list.appendChild(li);
+    }
+    console.log(casesList2021)
 });
 
 d3.json("Output/countries.json").then(function(data) {
@@ -22,3 +43,5 @@ d3.json("Output/rates.json").then(function(data) {
     let rates = data
     console.log(data)
 });
+
+
